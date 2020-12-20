c87d5d06de802022899feae0e5597958c44f2a56
Shorouq Fakhoury <Shorouq.fakhoury@gmail.com>
2020-12-20 14:21:50 +0200

    react Intro exercises

--- src/App.js
+++ src/App.js
@@ -3,19 +3,31 @@ import './App.css';
 
 class App extends Component {
   getStuff() {
+    return <h1>something in heading</h1>
   }
 
   getMorningGreeting() {
+    return <div>Good Morning</div>
   }
 
   getEveningGreeting() {
+    return <div>Good Evening</div>
   }
 
   showCompany(name, revenue) {
+    return <div id={name}>{name} makes {revenue} every year</div>
   }
 
   getClassName(temperature) {
-
+    let description
+    if(temperature < 15) {
+      description = "freezing"
+    }else if(temperature <= 30){
+      description = "fair"
+    }else{
+      description = "hell-scape"
+    }
+    return <div id="weatherBox" className={description}>{description}</div>
   }
 
   render() {
@@ -27,39 +39,45 @@ class App extends Component {
     ]
 
     return (
+      
       <div>
+        
         <div className="ex-space">
           <h4 className='ex-title'>Spot-check 1</h4 >
           <div className="exercise" id="spotcheck-1">
-            {/* your code here */}
+            {this.getStuff()}
           </div>
         </div>
 
         <div className="ex-space">
           <h4 className='ex-title'>Spot-check 2</h4>
           <div className="exercise" id="spotcheck-2">
-            {/* your code here */}
+            {new Date().getHours() > 10 ? this.getEveningGreeting() : this.getMorningGreeting()}
           </div>
         </div>
 
         <div className="ex-space">
           <h4 className='ex-title'>Spot-check 3</h4>
           <div className="exercise" id="spotcheck-3">
-            {/* your code here */}
+            {[
+              this.getMorningGreeting(),
+              this.getEveningGreeting(),
+              <p>some text</p>
+            ]}
           </div>
         </div>
 
         <div className="ex-space">
           <h4 className='ex-title'>Exercise 1</h4>
           <div className="exercise" id="ex-1">
-            {/* your code here */}
+            {companies.map(c => this.showCompany(c.name, c.revenue))}
           </div>
         </div>
 
         <div className="ex-space">
           <h4 className='ex-title'>Exercise 2</h4>
           <div className="exercise" id="ex-2">
-            {/* your code here */}
+            {this.getClassName(40)}
           </div>
         </div>
       </div>
